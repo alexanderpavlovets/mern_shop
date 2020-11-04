b6a5175041dbf7bd0600d05cd038fb543540b7c8
Alexander Pavlovets <aleksandrpavlovets90@gmail.com>
2020-11-04 23:48:04 +0200

    data for front-end is got from backend via useState useEffect hooks and axios

--- frontend/README.md
+++ frontend/README.md
@@ -1,6 +1,8 @@
 ## Notes 
 - we use bootswatch to get css theme - that's why we don't install bootstrap itself. only react-bootstrap
 
+- "proxy" property in /frontend/package.json - redirects from 3000 to 5000 port. Allows to write requests from Components just to "/api/smth" via axios in useEffect hook
+
 
 # Getting Started with Create React App
 
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import products from '../products'
+import axios from 'axios'
 
 const HomeScreen = () => {
+  // products = name, setProducts = function to handle their state
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+
+      setProducts(data)
+    }
+
+    fetchProducts()
+  }, []) // empty array for dependencies. Read about it mor.
+
   return (
     <>
       <h1>Latest products</h1>
@@ -21,3 +34,7 @@ const HomeScreen = () => {
 export default HomeScreen
 
 //  <Col sm={12} md={6} lg={4} xl={3}> - react -bootstrap GRID in docs
+
+// useState hook is instead of state in calss Componenets
+
+// useEffect hook - as soon as component loads
