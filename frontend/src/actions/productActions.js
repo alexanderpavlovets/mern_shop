05762b03b35980f2acdba76c31047b428381e6ac
Alexander Pavlovets <aleksandrpavlovets90@gmail.com>
2021-01-21 21:18:52 +0200

    Product details screen are now fetching data from redux

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,12 @@
 import axios from 'axios'
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from '../constants/productConstants.js'
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL
+} from '../constants/productConstants.js'
 
 // this is actionCreator function
 // this is where thunk comes in. TODO: read more about thunks - ahhh again these thunks.
@@ -16,6 +23,26 @@ export const listProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
+      payload: error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message 
+    })
+  }
+}
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST })
+
+    const { data } = await axios.get(`/api/products/${id}`)
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload: error.response && error.response.data.message
         ? error.response.data.message
         : error.message 
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,26 +1,27 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import Rating from '../components/Rating'
-import axios from 'axios'
+import { listProductDetails } from '../actions/productActions'
 
 const ProductScreen = ({ match }) => {
-  const [product, setProduct] = useState({})
+  const dispatch = useDispatch()
+
+  const productDetails = useSelector(state => state.productDetails)
+  const { loading, error, product } = productDetails
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${match.params.id}`)
-
-      setProduct(data)
-    }
-
-    fetchProduct()
-  }, [match]) // empty array for dependencies. Read about it more.
+    dispatch(listProductDetails(match.params.id))
+  }, [dispatch, match]) // array for dependencies. Read about it more.
 
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>Go Back</Link>
-      <Row>
+      {loading ? <Loader /> : error ? <Message  variant='danger'>{error}</Message> : (
+        <Row>
         <Col md={6}>
           <Image src={product.image} alt={product.name} fluid></Image>
         </Col>
@@ -81,6 +82,8 @@ const ProductScreen = ({ match }) => {
           </Card>
         </Col>
       </Row>
+      )}
+      
     </>
   )
 }
