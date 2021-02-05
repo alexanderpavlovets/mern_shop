d6e11188768a1aeb0409649b12c4e1f5b1167750
Alexander Pavlovets <aleksandrpavlovets90@gmail.com>
2021-02-05 17:07:44 +0200

    controllers for backend instead of logic in routes

---
+++ backend/controllers/productController.js
@@ -0,0 +1,30 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+// @desc    Fetch all products 
+// @route   GET /api/products 
+// @access  Public 
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}) // empty obj asparam gives everything 
+
+  res.json(products)
+})
+
+// @desc    Fetch single product 
+// @route   GET /api/products/:id
+// @access  Public 
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    res.json(product)
+  } else {
+    res.status(404)
+    throw new Error('Product not found') // due to we user error handler middleware - we can just throw errors
+  }
+})
+
+export {
+  getProducts,
+  getProductById,
+}
