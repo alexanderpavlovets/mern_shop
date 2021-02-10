d3df11f8dc44f3e977110806a31b4b09d8289d1f
Alexander Pavlovets <aleksandrpavlovets90@gmail.com>
2021-02-10 21:49:49 +0200

    getUserProfile and upateUserProfile backend is added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, registerUser, getUserProfile } from '../controllers/userController.js'
+import { authUser, registerUser, getUserProfile, updateUserProfile } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser)
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile) // this is an example hot to provide middleware to only 1 endpoint
+router.route('/profile')
+  .get(protect, getUserProfile) // this is an example hot to provide middleware to only 1 endpoint
+  .put(protect, updateUserProfile)
 
 export default router
