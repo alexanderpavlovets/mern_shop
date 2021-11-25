c89b8e33fb51659f8e597b15649941f5b5a6c102
alexanderpavlovets <aleksandrpavlovets90@gmail.com>
2021-11-25 17:27:41 +0200

    BE and redux preparation for PayPal integration.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -29,7 +29,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 })
 
-// @desc    get order by ID
+// @desc    Get order by ID
 // @route   GET /api/orders/:id
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
@@ -43,9 +43,37 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 })
 
+
+// @desc    Update order to paid
+// @route   PUT /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if(order) {
+    order.isPaid = true
+    order.paidAt = Date.now()
+    // these are from PayPal - want another one payment system - modify this.
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address
+    }
+
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
 export {
   addOrderItems,
-  getOrderById
+  getOrderById,
+  updateOrderToPaid
 }
 
 // .populate method - read more. Seems it filters what should be included in user. User is connected by Mongoose shema here
