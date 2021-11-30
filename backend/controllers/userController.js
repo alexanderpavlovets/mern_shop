af2bf8adc377fdd16657f5fbe30999034ba262dc
alexanderpavlovets <aleksandrpavlovets90@gmail.com>
2021-11-30 14:50:39 +0200

    admin update and get users endpoints are added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -123,7 +123,7 @@ const getUsers = asyncHandler(async (req, res) => {
 // @access  Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
 
-  const user = await User.findById(req.params.id) // empty object == all users
+  const user = await User.findById(req.params.id)
   
   if(user) {
     await user.remove()
@@ -134,6 +134,46 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc    Get user by ID  
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+
+  const user = await User.findById(req.params.id).select('-password')
+  if(user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+
+  const user = await User.findById(req.params.id) // here we have this req.user onject, because of "protect" middleware
+
+  if(user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User is not found')
+  }
+})
+
 
 export {
   authUser,
@@ -141,5 +181,7 @@ export {
   registerUser,
   updateUserProfile,
   getUsers,
-  deleteUser
+  deleteUser,
+  getUserById,
+  updateUser
 }
